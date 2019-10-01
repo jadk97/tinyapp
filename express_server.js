8da171ccdb9d8ba93209ae18f35b60b512dd811f
jad <jad_dis@hotmail.com>
2019-10-01 16:29:48 +0000

    added a delete button to the index page

--- express_server.js
+++ express_server.js
@@ -60,6 +60,12 @@ app.get("/u/:shortURL", (req, res) => {
 
     res.redirect(longURL);
 });
+
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
